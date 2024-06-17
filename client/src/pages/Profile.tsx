758dc578dbd615917f074de89d6d6542aff7ddd9
Ammarmyp <ammarmyp@gmail.com>
2024-06-17 13:55:08 +0300

    styling places and users list

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -17,10 +17,22 @@ interface UserType {
   gender: string;
 }
 
+interface PlaceType {
+  id: string;
+  name: string;
+  description: string;
+  location: string;
+  city: string;
+  region: string;
+  category: string;
+}
+
 const Profile: React.FC = () => {
   const username = Cookies.get("username");
   const isAdmin = username === "emailemail@gmail.com";
   const [users, setUsers] = useState<UserType[]>([]);
+  const [places, setPlaces] = useState<PlaceType[]>([]);
+  const [selectedOption, setSelectedOption] = useState<string>("users");
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
@@ -40,21 +52,38 @@ const Profile: React.FC = () => {
       }
     };
 
+    const fetchPlaces = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost/abyssinia-adventure/server/Report/getAllPlaces.php"
+        );
+        if (response.data.success) {
+          setPlaces(response.data.data);
+        } else {
+          setError("Failed to fetch places.");
+        }
+      } catch (error) {
+        console.error("Error fetching places:", error);
+        setError("An error occurred while fetching places.");
+      }
+    };
+
     if (isAdmin) {
       fetchUsers();
+      fetchPlaces();
     }
   }, [isAdmin]);
 
-  const columns = [
+  const columnsUsers = [
     {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
-      render: (text: string, record: UserType) => (
-        <Space size="middle">
-          {record.first_name} {record.last_name}
-        </Space>
-      ),
+      title: "First Name",
+      dataIndex: "first_name",
+      key: "first_name",
+    },
+    {
+      title: "Last Name",
+      dataIndex: "last_name",
+      key: "last_name",
     },
     {
       title: "Email",
@@ -87,31 +116,103 @@ const Profile: React.FC = () => {
     },
   ];
 
+  const columnsPlaces = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+    },
+    {
+      title: "Location",
+      dataIndex: "location",
+      key: "location",
+    },
+    {
+      title: "City",
+      dataIndex: "city",
+      key: "city",
+    },
+    {
+      title: "Region",
+      dataIndex: "region",
+      key: "region",
+    },
+    {
+      title: "Category",
+      dataIndex: "category",
+      key: "category",
+    },
+  ];
+
+  const handleOptionChange = (option: string) => {
+    setSelectedOption(option);
+  };
+
   return (
     <>
       <Navbar color="bg-gray-700" />
       <div className="min-h-screen bg-gray-100">
         <div className="flex justify-start items-start py-8 px-4">
           <div className="w-full max-w-screen-xl mx-auto">
-            <h1 className="text-3xl font-semibold mb-4 pt-10">
+            <h1 className="text-4xl font-bold text-gray-900 mb-6 pt-10">
               Welcome to the Admin Panel
             </h1>
             {username && (
-              <p className="text-lg mb-4">
-                Logged in as:{" "}
-                <span className="text-2xl font-bold text-gray-800">
+              <div className="flex items-center mb-6">
+                <p className="text-lg text-gray-700 mr-2">Logged in as:</p>
+                <span className="text-xl font-semibold text-gray-900 underline">
                   {username}
                 </span>
-              </p>
+              </div>
             )}
+
             {isAdmin ? (
               <div className="bg-white rounded-lg shadow-md overflow-hidden">
-                <Table
-                  dataSource={users}
-                  columns={columns}
-                  bordered
-                  pagination={{ pageSize: 10 }} // Adjust page size as needed
-                />
+                <div className="flex justify-between items-center bg-gray-200 px-4 py-2 mb-4 rounded-lg shadow-md">
+                  <button
+                    className={` text-base px-4 py-2 rounded-md transition-colors ${
+                      selectedOption === "users"
+                        ? "bg-green-500 text-white hover:bg-green-600"
+                        : "bg-gray-300 text-gray-800 hover:bg-gray-400"
+                    }`}
+                    onClick={() => handleOptionChange("users")}
+                  >
+                    Show List of All Users
+                  </button>
+                  <button
+                    className={`text-base px-4 py-2 rounded-md transition-colors ${
+                      selectedOption === "places"
+                        ? "bg-green-500 text-white hover:bg-green-600"
+                        : "bg-gray-300 text-gray-800 hover:bg-gray-400"
+                    }`}
+                    onClick={() => handleOptionChange("places")}
+                  >
+                    Show List of All Places
+                  </button>
+                </div>
+
+                {selectedOption === "users" ? (
+                  <Table
+                    dataSource={users}
+                    columns={columnsUsers}
+                    bordered
+                    pagination={{ pageSize: 10 }}
+                    key="usersTable"
+                  />
+                ) : (
+                  <Table
+                    dataSource={places}
+                    columns={columnsPlaces}
+                    bordered
+                    pagination={{ pageSize: 10 }}
+                    key="placesTable"
+                  />
+                )}
               </div>
             ) : (
               <p className="text-lg mt-4">
---
+++ server/Report/getAllPlaces.php
@@ -0,0 +1,38 @@
+<?php
+header("Access-Control-Allow-Origin: *"); // Allow requests from any origin
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// Include database configuration
+include_once '../config/database.php';
+
+// Initialize database connection
+$database = new Database();
+$db = $database->getConnection();
+
+if ($db === null) {
+    // Log the error and stop the execution
+    error_log("Database connection failed.");
+    die(json_encode(array("message" => "Database connection failed.")));
+}
+
+$query = "SELECT id, name, description, location, city, region, category  FROM place";
+
+$stmt = $db->prepare($query);
+
+if ($stmt === false) {
+    error_log("Failed to prepare statement: " . print_r($db->errorInfo(), true));
+    die(json_encode(array("message" => "Failed to prepare statement.")));
+}
+
+$stmt->execute();
+$places = array();
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $places[] = $row;
+}
+
+echo json_encode(array("success" => true, "data" => $places));
+
+$db = null;
